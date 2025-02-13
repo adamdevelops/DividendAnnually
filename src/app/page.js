dbc9c91c0121c208bb459fd2f6e35bd8d6934d1e
Adam Hussain <adamh93@verizon.net>
2025-02-13 00:39:31 -0500

    done: Build out basic UI

--- src/app/page.js
+++ src/app/page.js
@@ -2,94 +2,44 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const example_stocks = [
+    {id: 1, name: "VOO", shares_owned: 4, div_yield: "4 %"},
+    {id: 2, name: "APPL", shares_owned: 2, div_yield: "3 %"},
+    {id: 3, name: "SCHD", shares_owned: 1, div_yield: "7 %"},
+  ]
+
+
+  const renderStocks = example_stocks.map(stock => 
+    <li className="stock-item" key={stock.id}>           
+      <span>{stock.name}</span>
+      <span>{stock.shares_owned}</span>
+      <span>{stock.div_yield}</span>
+    </li>
+  );
+
+
+  return (
+    <div className="app">
+      <h1>Div Ann</h1>
+
+      <span>
+        <a className="menu-item">Home</a>
+        <a className="menu-item">About</a>
+        <a className="menu-item">Contact</a>
+      </span>
+
+      <div id="search-area" className="search-area">
+        <input placeholder="Type in name of stock to add" />
+        <button>Search</button>
+      </div>
+
+      <div>
+        <h3>My Portfolio</h3>
+        <ul>
+          {renderStocks}
+        </ul>
+      </div>
     </div>
   );
 }
